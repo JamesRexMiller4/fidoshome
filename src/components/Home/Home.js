026063dc8534a0cb57cfbc5b5cd426a2e690719f
James Miller <james.rex.miller.4@gmail.com>
2020-01-11 13:38:35 -0700

    Add apiCalls, Form, and Results folders + files

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,16 +5,16 @@ import PawLogo from '../../images/favicon-32x32.png';
 class Home extends Component {
 
   componentDidMount() {
-      fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        body: 'grant_type=client_credentials&client_id=fAK8vsY71R3Dkd8q7fAs76vYIpNB6Mpp4EY0huwEcQetNLY5FT&client_secret=1nvqj0muwVXgVvxrLcGrMapaRCMfIAR7BaMsYulQ',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      .then(res => res.json())
-      .then(data => sessionStorage.setItem('token', data.access_token))
-      .catch(error => console.log(error))
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials&client_id=fAK8vsY71R3Dkd8q7fAs76vYIpNB6Mpp4EY0huwEcQetNLY5FT&client_secret=1nvqj0muwVXgVvxrLcGrMapaRCMfIAR7BaMsYulQ',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    .then(res => res.json())
+    .then(data => sessionStorage.setItem('token', data.access_token))
+    .catch(error => console.log(error))
   }
 
   render() { 
