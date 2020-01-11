9569a62cfd576b3df5480b2134ee05c05c81c043
James Miller <james.rex.miller.4@gmail.com>
2020-01-11 11:09:30 -0700

    Add componentDidMount and fetch token for Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Home.scss';
+
+class Home extends Component {
+
+  componentDidMount() {
+      fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials&client_id=fAK8vsY71R3Dkd8q7fAs76vYIpNB6Mpp4EY0huwEcQetNLY5FT&client_secret=1nvqj0muwVXgVvxrLcGrMapaRCMfIAR7BaMsYulQ',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      .then(res => res.json())
+      .then(data => sessionStorage.setItem('token', data.access_token))
+      .catch(error => console.log(error))
+  }
+
+  render() { 
+    return ( 
+      <section>
+        <div className="header-wrapper">
+          <h2 className="home-h2">Fido's Home</h2>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Home;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Landing.scss';
 import Header from '../Header/Header';
+import { Link } from 'react-router-dom'; 
 
 const Landing = () => {
   return ( 
@@ -8,7 +9,9 @@ const Landing = () => {
       <Header />
       <section className='welcome-section'>
         <h2 id='welcome-1' className='landing-welcome-h2'>Give your new best friend</h2>
-        <button id='enter-btn' className='btn' type='button'>Enter</button>
+        <Link to='/home'>
+          <button id='enter-btn' className='btn' type='button'>Enter</button>
+        </Link>
         <h2 id='welcome-2' className='landing-welcome-h2'>the home they always dreamed of</h2>
       </section>
     </main>
