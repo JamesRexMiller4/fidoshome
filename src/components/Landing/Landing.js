ce20c1673050cf9093b63945e03507cf0db0610a
James Miller <james.rex.miller.4@gmail.com>
2020-01-11 10:24:50 -0700

    Add btn, animations, and styles

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,8 +6,9 @@ const Landing = () => {
   return ( 
     <main>
       <Header />
-      <section>
+      <section className='welcome-section'>
         <h2 id='welcome-1' className='landing-welcome-h2'>Give your new best friend</h2>
+        <button id='enter-btn' className='btn' type='button'>Enter</button>
         <h2 id='welcome-2' className='landing-welcome-h2'>the home they always dreamed of</h2>
       </section>
     </main>
