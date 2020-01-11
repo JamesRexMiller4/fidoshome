f8a299feea6271258f02d34329fde9d42ded082d
James Miller <james.rex.miller.4@gmail.com>
2020-01-11 14:17:00 -0700

    Add boilerplate to Results file

---
+++ src/containers/Results/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Results.scss';
+
+const Results = () => {
+  return ( 
+    <section>
+    <h2>Results</h2>
+    </section>
+  );
+}
+
+export default Results;
