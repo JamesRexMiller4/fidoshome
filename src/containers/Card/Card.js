13c30c8d6ee1a7e67e6e76627835225f308f8768
James Miller <james.rex.miller.4@gmail.com>
2020-01-12 08:25:49 -0700

    Add Link rtag to Card component - routing is successful

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Card.scss';
+import { Link } from 'react-router-dom';
 
-const Card = ({ name, address, img, age }) => {
-    return ( 
-      <div className='card-div'>
-        <img src={img} alt='shelter' className='shelter-photo'/>
-        <div className='div-details-body'>
-          <div className='div-details'>
-            <h3 className='pet-name-h3'>{name}</h3>
-            <div className='pet-card-details-wrapper'>
-            <h3 className='pet-age-h3'>{age}</h3>
-            <h3 className='pet-location-h3'>Location: {address}</h3>
+const Card = ({ name, address, img, age, id }) => {
+    return (
+      <Link to={`/${id}`}>
+        <div className='card-div'>
+          <img src={img} alt='shelter' className='shelter-photo'/>
+          <div className='div-details-body'>
+            <div className='div-details'>
+              <h3 className='pet-name-h3'>{name}</h3>
+              <div className='pet-card-details-wrapper'>
+              <h3 className='pet-age-h3'>{age}</h3>
+              <h3 className='pet-location-h3'>Location: {address}</h3>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 
