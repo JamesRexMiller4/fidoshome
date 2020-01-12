baa2e4a95c24fe70bb12d4276b0cb57ccf300b67
James Miller <james.rex.miller.4@gmail.com>
2020-01-12 16:22:48 -0700

    Add test files for apiCalls, reducer, and write passing tests for addAnimals reducer

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -4,12 +4,10 @@ import Header from './Header';
 
 describe('Header', () => {
   let wrapper;
-
   beforeEach(() => {
     wrapper = shallow(<Header />)
-  })
-
+  });
 it('should match the snapshot', () => {
   expect(wrapper).toMatchSnapshot();
-  })
+  });
 });
